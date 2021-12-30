f52cbcf15e78138a5ffc99f2b4f846acab848a91
lahbabic <lahbabic@gmail.com>
2021-12-30 19:44:23 +0100

    development

--- c_pipe.c
+++ c_pipe.c
@@ -3,26 +3,19 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <fcntl.h>
 
 typedef struct{
 	int *io_child;
     int *io_parent;
 	char *command;
 }args;	
-typedef struct{
-    char *command;
-    char *argve;
-    char *envp;
-}execveargs;
 
 int main(int argc, char const *argv[])
 { 
-    int threadid0, threadid1;
     char* argve[] = {};
 	char* envp[] = {};	
 	args *arg = (args *)malloc(sizeof(args));
-    execveargs *execvearg = (execveargs *)malloc(sizeof(execveargs));
 	arg->io_child = (int *)malloc(2*sizeof(int));	
 	arg->io_parent = (int *)malloc(2*sizeof(int));	
 	char *command = "/bin/python3\0";
@@ -34,8 +27,6 @@ int main(int argc, char const *argv[])
 	int nbytes = 0;
     int rbytes = 0;
 	char *buffer = (char *)malloc(sizeof(char));
-    //char ch[1];
-    int status = 0;
     int fork_pid = fork();
     if (fork_pid < 0)
     {
@@ -44,27 +35,20 @@ int main(int argc, char const *argv[])
     }
     else if (fork_pid == 0)
     {
-        pthread_create(&threadid0, NULL, pipe, &arg->io_child);
-        //dup2(arg->io_child[1], 0);
-        //dup2(1, arg->io_child[0]);
-        execvearg->command = command;
-        //*execvearg->argve = {};
-        //*execvearg->envp = {};
-        pthread_create(&threadid1, NULL, execve, &execvearg);
+        pipe(arg->io_child);
+        dup2(arg->io_child[1], 0);
+        dup2(arg->io_child[0], 1);
+        arg->io_child[1] = fcntl(arg->io_child[1], F_SETFD, 0);
+        arg->io_child[0] = fcntl(arg->io_child[0], F_SETFD, 0);
+        execve(arg->command, argve, envp);
     }else
     {   
-        pipe(arg->io_parent);
         char *com = "2**4\n";
         for(int i = 0; i < strlen(com); ++i)
         {
-            write(0, &com[i], sizeof(com[i]));
+            write(arg->io_child[1], &com[i], sizeof(com[i]));
         }
-        //if (waitpid(fork_pid, &status, 0) < 0)
-        //    perror("Waitpid error.");
-        printf("status = %d", status);
-        //close(arg->io_parent[1]);
-        //dup2(arg->io_parent[0], 1);
-        while ((rbytes = read(1, &buffer[nbytes], sizeof(buffer[nbytes]))) > 0){
+        while ((rbytes = read(arg->io_child[0], &buffer[nbytes], sizeof(buffer[nbytes]))) > 0){
             nbytes++;
             buffer = (char *)realloc(buffer, nbytes+1); 
         }
